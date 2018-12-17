ef4b466c3fa167900a9a0229cbedb4b403a88e4a
Tyler Reardon <tdr.reardon@gmail.com>
2018-12-17 10:50:14 -0800

    Heroku config

--- api/router.js
+++ api/router.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const fs = require('fs'); // eslint-disable-line global-require
 
-const creds = JSON.parse(fs.readFileSync('./creds.json'));
-const knex = require('knex')({
-  client: 'mysql',
-  connection: {
-    host: creds.host,
-    user: creds.user,
-    password: creds.password,
-    database: creds.database,
-  },
-});
+// const creds = JSON.parse(fs.readFileSync('./creds.json'));
+// const knex = require('knex')({
+//   client: 'mysql',
+//   connection: {
+//     host: creds.host,
+//     user: creds.user,
+//     password: creds.password,
+//     database: creds.database,
+//   },
+// });
 
 // knex.on('query-response', function(result, obj, builder) {
 //   console.log('query:    ', obj.sql);
@@ -22,8 +22,8 @@ const knex = require('knex')({
 const router = express.Router(); // eslint-disable-line new-cap
 
 // import user routes
-const user = require('./controllers/user');
+// const user = require('./controllers/user');
 
-router.get('/users', (req, res) => user.getUser(req, res, knex));
+// router.get('/users', (req, res) => user.getUser(req, res, knex));
 
 module.exports = router;
